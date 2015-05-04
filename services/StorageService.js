3fe52e5f4fa631e0d52960a95090ba48485b4bd8
sakshi <sakshi@intelligrape.com>
2015-05-04 13:35:10 +0530

    Modified upload api for multiple file uploads

--- services/StorageService.js
+++ services/StorageService.js
@@ -1,24 +1,40 @@
 var fs = require('fs');
 var EventName = require('../src/enum/EventName');
+var async = require('async');
 
-module.exports.upload = function (file){
+module.exports.upload = function (files){
     var emitter = this;
-    if (file) {
-        var name = (+new Date()) + file.hapi.filename;
-        var path = __appBaseDir + "/uploads/" + name;
-        var image = fs.createWriteStream(path);
-
-        image.on('error', function (err){
-            console.error(err)
+    if(!(files instanceof Array)){
+        files = [files];
+    }
+   if (files && files.length) {
+        var tasks = [];
+        files.forEach(function (file){
+            tasks.push(function (cb){
+                uploadImage(file,cb);
+            });
         });
-
-        file.pipe(image);
-
-        file.on('end', function (err){
-            if (err)
-                emitter.emit(EventName.ERROR, err);
+        async.parallel(tasks, function(err, result){
+            if(err)
+            emitter.emit(EventName.ERROR, err);
             else
-                emitter.emit(EventName.DONE,{path : name });
-        })
+                emitter.emit(EventName.DONE, result);
+        });
     }
 }.toEmitter();
+
+function uploadImage(file,cb){
+    var name = (+new Date()) + file.hapi.filename;
+    var path = __appBaseDir + "/uploads/" + name;
+    var image = fs.createWriteStream(path);
+
+    image.on('error', function (err){
+        cb(err);
+    });
+
+    file.pipe(image);
+
+    file.on('end', function (err){
+        cb(err, name);
+    })
+}
