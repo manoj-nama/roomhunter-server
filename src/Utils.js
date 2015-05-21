96bbeca3591ef2fa2dc4a2f1e2c1151b2c480e15
sakshi <sakshi@intelligrape.com>
2015-05-21 12:21:39 +0530

    Implemented paginated response according to offset passed in search room api

--- src/Utils.js
+++ src/Utils.js
@@ -5,12 +5,12 @@ var crypto = require('crypto'),
 
 exports.encrypt = function (text){
     var cipher = crypto.createCipher(algorithm, password);
-    try{
+    try {
         var crypted = cipher.update(text, 'utf8', 'hex');
         crypted += cipher.final('hex');
         return crypted;
     }
-    catch(e){
+    catch (e) {
         return null;
     }
 
@@ -18,19 +18,20 @@ exports.encrypt = function (text){
 
 exports.decrypt = function (text){
     var decipher = crypto.createDecipher(algorithm, password);
-    try{
+    try {
         var dec = decipher.update(text, 'hex', 'utf8');
         dec += decipher.final('utf8');
         return dec;
     }
-    catch(e){
+    catch (e) {
         return null;
     }
 
 };
 
-exports.parseFilterUrl = function (filters) {
+exports.parseFilterUrl = function (filters){
     var criteria = {};
+    var offset = 0;
     for (var key in filters) {
         if (filters.hasOwnProperty(key)) {
             switch (key) {
@@ -50,8 +51,11 @@ exports.parseFilterUrl = function (filters) {
                         criteria['propertyType'] = {$in: filters[key].split('_')};
                     }
                     break;
+                case 'offset' :
+                    offset = filters[key];
+                    break;
             }
         }
     }
-    return criteria;
+    return {criteria: criteria, offset: offset};
 };
